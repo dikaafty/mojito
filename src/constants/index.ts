c2b4d1411d9a9ec2a670056c2a50e8c7f16dc991
Dika Afty <dikaafty@gmail.com>
2025-09-23 20:07:40 +0700

    Create storeInfo array with its items and export it

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -100,6 +100,15 @@ const goodLists = [
  "Freshly muddled flavors",
 ];
 
+const storeInfo = {
+ heading: "Where to Find Us",
+ address: "456, Raq Blvd. #404, Los Angeles, CA 90210",
+ contact: {
+	phone: "(555) 987-6543",
+	email: "hello@jsmcocktail.com",
+ },
+};
+
 export {
   navLinks,
   cocktailLists,
@@ -107,4 +116,5 @@ export {
   profileLists,
   featureLists,
   goodLists,
+  storeInfo,
 }
