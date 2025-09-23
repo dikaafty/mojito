2ca09c18113d7f0ce83f63185135028db858b44d
Dika Afty <dikaafty@gmail.com>
2025-09-23 20:44:48 +0700

    Import Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { gsap } from "gsap";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
