6d0b47d110c652ebb20303fe2b7e926c8e49300e
Dika Afty <dikaafty@gmail.com>
2025-09-23 20:08:52 +0700

    Create openingHours array with its items and export it

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -109,6 +109,13 @@ const storeInfo = {
  },
 };
 
+const openingHours = [
+ { day: "Mon–Thu", time: "11:00am – 12am" },
+ { day: "Fri", time: "11:00am – 2am" },
+ { day: "Sat", time: "9:00am – 2am" },
+ { day: "Sun", time: "9:00am – 1am" },
+];
+
 export {
   navLinks,
   cocktailLists,
@@ -117,4 +124,5 @@ export {
   featureLists,
   goodLists,
   storeInfo,
+  openingHours,
 }
