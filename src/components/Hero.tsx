16d0b9b1fe6d56e69902f557cfe195d11250de0b
Dika Afty <dikaafty@gmail.com>
2025-09-24 22:48:23 +0700

    Create startValue var with its value in declared useGSAP hook in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,6 +40,8 @@ const Hero = () => {
     })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
   }, []);
 
   return (
