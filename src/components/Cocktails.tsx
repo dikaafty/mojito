2a6a68d6a30d8c3ba9fa92e99df952d908ff0b46
Dika Afty <dikaafty@gmail.com>
2025-09-25 23:25:27 +0700

    Create cocktails section and its items jsx with their attributes in Cocktails component

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -4,8 +4,58 @@ import gsap from "gsap";
 
 const Cocktails = () => {
   return (
-    <section>
-      
+    <section id="cocktails" className="noisy">
+      <img 
+        src="/images/cocktail-left-leaf.png" 
+        alt="cocktail left leaf" 
+        id="c-left-leaf"  
+      />
+
+      <img 
+        src="/images/cocktail-right-leaf.png" 
+        alt="cocktail right leaf" 
+        id="c-right-leaf"  
+      />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails:</h2>
+
+          <ul>
+            {
+              cocktailLists.map(({ name, country, detail, price }) => (
+                <li key={name}>
+                  <div className="md:me-28">
+                    <h3>{name}</h3>
+                    <p>{country} | {detail}</p>
+                  </div>
+
+                  <span>- {price}</span>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Most loved cocktails:</h2>
+
+          <ul>
+            {
+              mockTailLists.map(({ name, country, detail, price }) => (
+                <li key={name}>
+                  <div className="me-28">
+                    <h3>{name}</h3>
+                    <p>{country} | {detail}</p>
+                  </div>
+
+                  <span>- {price}</span>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
