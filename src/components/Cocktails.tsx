ed7db7f3495818cc1bb89b0f8d247f80dc421f7b
Dika Afty <dikaafty@gmail.com>
2025-09-25 23:34:23 +0700

    Create parallax timeline in declared useGSAP hook in Cocktails component

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -4,7 +4,14 @@ import gsap from "gsap";
 
 const Cocktails = () => {
   useGSAP(() => {
-    
+    const parallaxTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      }
+    });
   }, []);
 
   return (
