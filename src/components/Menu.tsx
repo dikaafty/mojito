8ce6e3bdeab7bf14a730fcbdf36073bd438f0c95
Dika Afty <dikaafty@gmail.com>
2025-09-27 16:48:28 +0700

    Create currentIndex state and its setter in Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,8 @@ import { sliderLists } from "../constants";
 import { useState, useRef } from "react";
 
 const Menu = () => {
+  const [ currentIndex, setCurrentIndex ] = useState<number>(0);
+
   return (
     <section>
       
