190508ad65cd1f1a7c11b5945175433b7cb368d1
Dika Afty <dikaafty@gmail.com>
2025-09-28 14:42:20 +0700

    Create GSAP animation for contact right leaf in declared useGSAP hook in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,7 +17,8 @@ const Contact = () => {
 
     tl
       .from(titleSplit.words, { opacity: 0, yPercent: 100, stagger: 0.02 })
-      .from("#contact h3, #contact p", { opacity: 0, yPercent: 100, stagger: 0.02 });
+      .from("#contact h3, #contact p", { opacity: 0, yPercent: 100, stagger: 0.02 })
+      .to("#f-right-leaf", { y: -50, duration: 1, ease: "power1.inOut", });
   }, []);
 
   return (
